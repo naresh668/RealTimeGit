16b3e52a730dea52e86153bb6aad64a88d04e672
naresh <nareshreddylgm@gmail.com>
2021-06-04 07:24:32 +0530

    ggg

---
+++ src/main/java/com/example/demo/KafkaConsumerApplication.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
+
+@SpringBootApplication
+@RestController
+public class KafkaConsumerApplication {
+	
+	List<String> messages = new ArrayList<>();
+
+	User userFromTopic = null;
+
+	@GetMapping("/consumeStringMessage")
+	public List<String> consumeMsg() {
+		return messages;
+	}
+
+	@GetMapping("/consumeJsonMessage")
+	public User consumeJsonMessage() {
+		return userFromTopic;
+	}
+
+	@KafkaListener(groupId = "javatechie-1", topics = "publish", containerFactory = "kafkaListenerContainerFactory")
+	public List<String> getMsgFromTopic(String data) {
+		messages.add(data);
+		return messages;
+	}
+
+	@KafkaListener(groupId = "javatechie-2", topics = "publish", containerFactory = "userKafkaListenerContainerFactory")
+	public User getJsonMsgFromTopic(User user) {
+		userFromTopic = user;
+		return userFromTopic;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerApplication.class, args);
+	}
+}
